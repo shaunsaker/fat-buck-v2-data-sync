c4991b1281170161d257729414a287fd120d2453
Shaun Saker <sakershaun@gmail.com>
2020-09-25 15:46:18 +0200

    reenable funcs

--- src/main.ts
+++ src/main.ts
@@ -20,42 +20,42 @@ import { saveExchangeRates } from './firebase/saveExchangeRates';
 
 const main = async () => {
   console.log('Starting.');
-  // const activeBotId = await getActiveBotId();
+  const activeBotId = await getActiveBotId();
 
-  // // test if the server is up
-  // let isAlive = false;
-  // try {
-  //   isAlive = await getIsAlive();
-  // } catch (error) {
-  //   console.log('Server error.', error);
-  // }
+  // test if the server is up
+  let isAlive = false;
+  try {
+    isAlive = await getIsAlive();
+  } catch (error) {
+    console.log('Server error.', error);
+  }
 
-  // await saveIsAlive(isAlive, activeBotId);
+  await saveIsAlive(isAlive, activeBotId);
 
-  // if (!isAlive) {
-  //   console.log('Not alive. Stopping.');
-  //   process.exit();
-  // }
+  if (!isAlive) {
+    console.log('Not alive. Stopping.');
+    process.exit();
+  }
 
-  // const accessToken = await getAccessToken();
+  const accessToken = await getAccessToken();
 
-  // console.log('Getting trades.');
-  // const trades = await getTrades(accessToken);
-  // console.log('Getting open trades.');
-  // const openTrades = await getOpenTrades(accessToken); // aka open trades
-  // const allTrades = [...trades, ...openTrades];
-  // console.log('Saving trades.');
-  // await saveTrades(allTrades, activeBotId);
+  console.log('Getting trades.');
+  const trades = await getTrades(accessToken);
+  console.log('Getting open trades.');
+  const openTrades = await getOpenTrades(accessToken); // aka open trades
+  const allTrades = [...trades, ...openTrades];
+  console.log('Saving trades.');
+  await saveTrades(allTrades, activeBotId);
 
-  // console.log('Getting profit.');
-  // const profit = await getProfit(accessToken);
-  // console.log('Saving profit.');
-  // await saveProfit(profit, activeBotId);
+  console.log('Getting profit.');
+  const profit = await getProfit(accessToken);
+  console.log('Saving profit.');
+  await saveProfit(profit, activeBotId);
 
-  // console.log('Getting balance.');
-  // const balance = await getBalance(accessToken);
-  // console.log('Saving balance.');
-  // await saveBalance(balance, activeBotId);
+  console.log('Getting balance.');
+  const balance = await getBalance(accessToken);
+  console.log('Saving balance.');
+  await saveBalance(balance, activeBotId);
 
   console.log('Getting exchange rates.');
   const exchangeRates = await getExchangeRates();

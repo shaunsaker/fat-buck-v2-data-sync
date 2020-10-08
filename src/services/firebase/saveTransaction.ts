c4cbb191ce3ad1b7b438a802aa0210acf9489ee0
Shaun Saker <sakershaun@gmail.com>
2020-10-08 07:21:30 +0200

    save transaction to users collection

--- src/services/firebase/saveTransaction.ts
+++ src/services/firebase/saveTransaction.ts
@@ -6,5 +6,13 @@ export const saveTransaction = async (
 ): Promise<null> => {
   await firebase.firestore().collection('transactions').doc().set(transaction);
 
+  await firebase
+    .firestore()
+    .collection('users')
+    .doc(transaction.uid)
+    .collection('transactions')
+    .doc()
+    .set(transaction);
+
   return null;
 };
